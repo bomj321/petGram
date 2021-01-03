93ffa4eac7bbc3d859f563c2a8367d5ff1f40d56
bomj321 <jmob612@gmail.com>
2021-01-03 06:27:37 -0400

    ADD: new text

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 
 ReactDom.render(
-  <h1>Seguimos con el curso avanzando!</h1>,
+  <h1>Seguimos con el curso avanzando con Vercel!</h1>,
   document.getElementById("app")
 );
