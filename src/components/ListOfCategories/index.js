275f2eb49e0b9971ebfc98c2b40a8d6f089fc9b5
bomj321 <jmob612@gmail.com>
2021-01-04 10:17:00 -0400

    ADD: Test two

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
 import { categories } from "../../../api/db.json";
-
 export const ListOfCategories = () => {
   return (
     <List>
