00f1312ccd49bc2760850cbfb49bd05932a8a000
bomj321 <jmob612@gmail.com>
2021-01-04 10:15:36 -0400

    ADD: new styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-ReactDom.render(
-  <h1>Seguimos con el curso avanzando con Vercel!</h1>,
-  document.getElementById("app")
-);
+import { App } from "./App";
+ReactDom.render(<App />, document.getElementById("app"));
